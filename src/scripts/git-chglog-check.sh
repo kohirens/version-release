50033553bda4e7a9102b16a9398292039d5a2564
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2021-10-13 05:32:57 -0400

    add: git-chglog check command.

---
+++ src/scripts/git-chglog-check.sh
@@ -0,0 +1,15 @@
+ConfigCheck() {
+    if [ -f "${PARAM_CONFIGFILE}" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    ConfigCheck
+    exit $?
+fi
