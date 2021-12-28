49111a62309526060751dda57d64da21d5888b7b
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2021-12-28 02:04:53 -0500

    add: Check for tags before updating CHANGELOG.md.

--- src/scripts/git-chglog-update.sh
+++ src/scripts/git-chglog-update.sh
@@ -1,7 +1,15 @@
 GitChglogUpdate() {
-    wd=$(pwd)
-    printf "updating %s using config file %s\n" "${wd}/${PARAM_OUTPUTFILE}" "${PARAM_CONFIGFILE}"
-    git-chglog --output "${PARAM_OUTPUTFILE}" -c "${PARAM_CONFIGFILE}"
+    hasTag=$(git tag)
+
+    printf "hasTag = \"%s\"" "$hasTag"
+
+    if [ -z "${hasTag}" ]; then
+        git-chglog --output "${PARAM_OUTPUTFILE}" -c "${PARAM_CONFIGFILE}" --next-tag=0.1.0
+    else
+        wd=$(pwd)
+        printf "updating %s using config file %s\n" "${wd}/${PARAM_OUTPUTFILE}" "${PARAM_CONFIGFILE}"
+        git-chglog --output "${PARAM_OUTPUTFILE}" -c "${PARAM_CONFIGFILE}"
+    fi
 }
 
 # Will not run if sourced for bats-core tests.
