88d407a9fd67a988f214e6973195a37d402bf6d6
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-01-04 02:11:06 -0500

    chg: Updated changelog command to use git-tool-belt.

--- src/scripts/git-chglog-update.sh
+++ src/scripts/git-chglog-update.sh
@@ -5,6 +5,10 @@ GitChglogUpdate() {
 
     if [ -z "${hasTag}" ]; then
         git-chglog --output "${PARAM_OUTPUTFILE}" -c "${PARAM_CONFIGFILE}" --next-tag=0.1.0
+    elif [ -f "/usr/local/bin/git-tool-belt" ]; then
+        git-tool-belt version
+        nextVersion=$(jq .nextVersion < build-version.json)
+        git-chglog --output "${PARAM_OUTPUTFILE}" -c "${PARAM_CONFIGFILE}" --next-tag="${nextVersion}"
     else
         wd=$(pwd)
         printf "updating %s using config file %s\n" "${wd}/${PARAM_OUTPUTFILE}" "${PARAM_CONFIGFILE}"
