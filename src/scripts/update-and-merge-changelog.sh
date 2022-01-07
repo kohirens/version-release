b445d2575860863dfacb1d3b6365fcdc40f642ae
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-01-07 00:13:49 -0500

    add: Flag to auto generate title and notes changelog merge.

--- src/scripts/update-and-merge-changelog.sh
+++ src/scripts/update-and-merge-changelog.sh
@@ -21,7 +21,7 @@ UpdateAndMergeChangelog() {
         sleep 5
         nextVersion=$(jq .nextVersion < build-version.json)
         releaseDay=$(date +"%Y-%m-%d")
-        gh release create "${nextVersion}" --target "${PARAM_BRANCH}" --title "[${nextVersion}] - ${releaseDay}"
+        gh release create "${nextVersion}" --generate-notes --target "${PARAM_BRANCH}" --title "[${nextVersion}] - ${releaseDay}"
     fi
 }
 
