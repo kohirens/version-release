843af88684561ebe50431670c6974eb613c2f524
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-02-03 09:53:47 -0500

    chg: Refactored merge-changelog.sh.

--- src/scripts/merge-changelog.sh
+++ src/scripts/merge-changelog.sh
@@ -14,8 +14,8 @@ MergeChangelog() {
     git config --global user.name "${CIRCLE_USERNAME}"
     git config --global user.email "${CIRCLE_USERNAME}@users.noreply.github.com"
     git checkout -b "${GEN_BRANCH_NAME}"
-    gentBranchCommitMsg=$(printf "Updated the %s\n\n[skip ci]" "${PARAM_CHANGELOG_FILE}")
-    git commit -m "${gentBranchCommitMsg}"
+    mergeBranchCommitMsg="Updated the ${PARAM_CHANGELOG_FILE}"
+    git commit -m "${mergeBranchCommitMsg}" -m "[skip ci]"
     # Do not run when sourced for bats-core
     # TODO: This can be tested if you mock the gh, git, and setup a dummy repo at test time.
     if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
@@ -33,34 +33,32 @@ MergeChangelog() {
 waitForPrToMerge() {
     # Wait until the branch is fully merged. and the merge branch has been updated.
     # This will help make sure that operations started in this job complete before moving on.
-    # 1. Record the commit has from committing the changelog
-    prCommit=$(git rev-parse "${GEN_BRANCH_NAME}")
-    # 2. Fetch remote changes
-    git fetch --all -p
-    echo "prevCommit = ${prevCommit}"
-    echo "prCommit = ${prCommit}"
     printf "merging pr is "
-    # 3. Loop for so many seconds
+    # 1. Loop for so many seconds
     counter=0
     while [ $counter -lt 10 ]; do
-        # 4. Get the latest commit of the merge branch
+        # 2. Fetch remote changes
+        git fetch --all -p
+        # 3. Get the latest commit of the merge branch
         currCommit=$(git rev-parse origin/"${PARAM_BRANCH}")
-        echo "currCommit = ${currCommit}"
-        # 5. Check to see if the merge branch previous commit and current commit have changed.
+        # 4. Check to see if the merge branch previous commit and current commit have changed.
         if [ "${currCommit}" != "${prevCommit}" ]; then
             echo " done"
             currCommitMsg=$(git show-branch --no-name "${currCommit}")
-            if [ "${currCommitMsg}" = "${gentBranchCommitMsg}" ]; then
+            # 5. If the commit messages are the same, then make a file to persist to the next job
+            if [ "${currCommitMsg}" = "${mergeBranchCommitMsg}" ]; then
                 echo "merge has completed successfully"
                 echo "${currCommit}" > "${PARAM_COMMIT_FILE}"
                 cm=$(cat "${PARAM_COMMIT_FILE}")
                 printf "cm = %s"$'\n' "${cm}"
             fi
+            # Exit the loop
+            break
         else
             printf "."
         fi
         counter=$((counter+1))
-        # wait a second
+        # Wait a second
         sleep 1
     done
 }
