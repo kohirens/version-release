9d813f400cae24c91e25b62716bed1542843372c
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-04-21 01:05:35 -0400

    Upgraded Git-Tool-Belt Image
    Upgrade git-tool-belt to check and, if missing, add a Git-ChgLog
    configuration.
    
    chg: Upgraded the git-tool-belt image.
    chg: Upgraded the git-chglog config check command.

--- src/scripts/add-missing-chglog-config.sh
+++ src/scripts/add-missing-chglog-config.sh
@@ -1,20 +1,5 @@
 AddMissingChgLogConfig() {
-    wd=$(pwd)
-    echo "working directory = ${wd}"
-
-    if [ ! -f "${PARAM_CONFIGFILE}" ]; then
-        CONFIG_DIR=$(dirname "${PARAM_CONFIGFILE}")
-        echo "no changelog file found at ${CONFIG_DIR}. adding the default..."
-        mkdir -p "${CONFIG_DIR}"
-        sed -e "s/\${VCS_URL}/${CIRCLE_REPOSITORY_URL}/" ./files/config.yml > "${PARAM_CONFIGFILE}"
-        cp ./files/CHANGELOG.tpl.md "${CONFIG_DIR}"
-
-        if [ ! -f "${PARAM_CONFIGFILE}" ]; then
-            echo "unable to add ${PARAM_CONFIGFILE}"
-        fi
-    else
-        echo "found git-chglog config here ${PARAM_CONFIGFILE}"
-    fi
+    git-tool-belt checkConf -path "${PARAM_CONFIGFILE}" -repo "${CIRCLE_REPOSITORY_URL}"
 }
 
 # Will not run if sourced for bats-core tests.
