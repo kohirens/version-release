de2d415acdb9454a3f3f614f8beb64e29135810c
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-10-01 16:32:21 -0400

    chg: Make Shell if statements Consistent

--- src/scripts/check-last-commit-updated-changelog.sh
+++ src/scripts/check-last-commit-updated-changelog.sh
@@ -19,6 +19,6 @@ CheckLastCommitUpdatedChangelog() {
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
-if [ "${0#*"${ORB_TEST_ENV}"}" == "$0" ]; then
+if [ "${0#*"${ORB_TEST_ENV}"}" = "$0" ]; then
     CheckLastCommitUpdatedChangelog
 fi
--- src/scripts/publish-changelog.sh
+++ src/scripts/publish-changelog.sh
@@ -103,6 +103,6 @@ waitForPrToMerge() {
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
-if [ "${0#*"${ORB_TEST_ENV}"}" == "$0" ]; then
+if [ "${0#*"${ORB_TEST_ENV}"}" = "$0" ]; then
     MergeChangelog
 fi
