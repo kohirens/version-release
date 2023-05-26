386338b536ad94b25ab818929f5b5396912916f9
Khalifah K. Shabazz <shabazzk@gmail.com>
2023-05-25 22:25:50 -0400

    chg: Over To Use Version Release Orb CLI
    Integrate the Version Release Orb CLI tool to replace bash scripts.
    All this is in an effort to fix bugs causing workflows to trigger
    unexpectidly. Causing confusion as to what was happening. Making
    the workflow-selector the only workflow that gets triggered when code is
    merged into the trunk branch. This allows more precise control as to
    what is triggerd and how, replacing the confusing distributed model.
    
    The following changes may also be breaking changes:
    
    * Added variable to toggle attachment of the workspace before any other
      steps in the publish-changelog job.
    * Removed/added/renamed job parameters.
    * Removed publish-changelog.sh script.
    * Updated documentation to reflect the changes in the interface.
    * Removed txt file use to indicate to the publish-changelog job that it
      should trigger a release tag workflow.
    * Added CI job to publish container image for version release, which
      replaces the git-tool-belt image that was the default executor.
    * Changed workflows to trigger when trigger_flow parameter is set.
    * Added a custom mock-server to run test that require a remote server
      response.
    * Set which version of the Version Release orb to use.
    
    BREAKING CHANGE
    
    * Removed jobs, commands, and examples along with their companion scripts.

--- src/scripts/publish-changelog.sh
+++ src/scripts/publish-changelog.sh
@@ -1,116 +1,14 @@
-MergeChangelog() {
-    # require a GH_TOKEN
-    if [ -z "${GH_TOKEN}" ]; then
-        echo "No GitHub write token found."
-        echo "Please set the environment variable GH_TOKEN."
-        echo "You can also specify which environment variable to use, see \"gh_token_var\" parameter in the publish-changelog job."
-        exit 1
-    fi
-
-    prevCommit=$(git rev-parse origin/"${PARAM_BRANCH}")
-
-    # make the file so that the persist step does not fail.
-    echo "" > "${PARAM_COMMIT_FILE}"
-
-    changelogUpdated=$(git diff --name-only -- "${PARAM_CHANGELOG_FILE}")
-    changelogUntracked=$(git status | grep "${PARAM_CHANGELOG_FILE}" || echo "")
-    if [ "${changelogUntracked}" != "" ]; then
-        echo "detected updates to the ${PARAM_CHANGELOG_FILE} file"
-    elif [ -z "${changelogUpdated}" ]; then
-        echo "no changes detected in the ${PARAM_CHANGELOG_FILE} file"
-        exit 0
-    fi
-
-    # Exit if branch exist remotely already.
-    GEN_BRANCH_NAME="auto-update-changelog"
-    branchExistRemotely=$(git ls-remote --heads "${CIRCLE_REPOSITORY_URL}" "${GEN_BRANCH_NAME}" | wc -l)
-    echo "branchExistRemotely = ${branchExistRemotely}"
-    # Exit if branch exist remotely already.
-    if [ "${branchExistRemotely}" = "1"  ]; then
-        echo "the branch '${GEN_BRANCH_NAME}' exists on ${CIRCLE_REPOSITORY_URL}, please remove it manually so this job can complete successfully; exiting with code 1"
-        exit 1
-    fi
-
-    # Piggy-back: The previous step command may have added the Git-ChgLog configuration. Lets commit
-    # here to reduce duplicating the commit code there.
-    gitChgLogConfigDir=$(dirname "${PARAM_CONFIG_FILE}")
-    git add CHANGELOG.md "${gitChgLogConfigDir}"
-    git status
-    git config --global user.name "${CIRCLE_USERNAME}"
-    git config --global user.email "${CIRCLE_USERNAME}@users.noreply.${PARAM_GH_SERVER}"
-    git checkout -b "${GEN_BRANCH_NAME}"
-    mergeBranchCommitMsg="Updated the ${PARAM_CHANGELOG_FILE}"
-    git commit -m "${mergeBranchCommitMsg}" -m "automated update of ${PARAM_CHANGELOG_FILE}"
-    # Do not run when sourced for bats-core
-    # TODO: This can be tested if you mock the gh, git, and setup a dummy repo at test time.
-    if [ "${0#*"${ORB_TEST_ENV}"}" == "$0" ]; then
-        git push origin "${GEN_BRANCH_NAME}"
-        # Switch to SSH to use the token stored in the environment variable GH_TOKEN.
-        gh config set git_protocol ssh --host "${PARAM_GH_SERVER}"
-        echo
-        echo
-        # see: https://josh-ops.com/posts/gh-auth-login-in-actions/
-        # NOTE: Using just GH_TOKEN set in the environment seems to fail when you have
-        #       to supply the --hostname flag to point to a GHE server.
-        if [ "${PARAM_GH_SERVER}" != "github.com" ]; then
-            echo "login to ${PARAM_GH_SERVER}"
-            echo "${GH_TOKEN}" | gh auth login --hostname "${PARAM_GH_SERVER}" --with-token
-            echo
-            echo
-        fi
-        echo "auth status of ${PARAM_GH_SERVER}"
-        gh auth status --hostname "${PARAM_GH_SERVER}"
-        echo
-        echo
-        echo "Making a PR"
-        gh pr create --base "${PARAM_BRANCH}" --head "${GEN_BRANCH_NAME}" --fill
-        sleep 5
-        gh pr merge --auto "--${PARAM_MERGE_TYPE}"
-
-        waitForPrToMerge
-        sleep 3
-        echo "trigger-tag-and-release" > trigger.txt
-        echo
-        echo
-    fi
-}
-
-waitForPrToMerge() {
-    # Wait until the branch is fully merged. and the merge branch has been updated.
-    # This will help make sure that operations started in this job complete before moving on.
-    printf "%s" "merging pr is "
-    # 1. Loop for so many seconds
-    counter=0
-    while [ $counter -lt 10 ]; do
-        # 2. Fetch remote changes
-        git fetch --all -p
-        # 3. Get the latest commit of the merge branch
-        currCommit=$(git rev-parse origin/"${PARAM_BRANCH}")
-        # 4. Check to see if the merge branch previous commit and current commit have changed.
-        if [ "${currCommit}" != "${prevCommit}" ]; then
-            echo " done"
-            currCommitMsg=$(git show-branch --no-name "${currCommit}")
-            # 5. If the commit messages are the same, then make a file to persist to the next job
-            if [ "${currCommitMsg}" = "${mergeBranchCommitMsg}" ]; then
-                echo "merge has completed successfully"
-                echo "${currCommit}" > "${PARAM_COMMIT_FILE}"
-                cm=$(cat "${PARAM_COMMIT_FILE}")
-                printf "cm = %s"$'\n' "${cm}"
-            fi
-            # Exit the loop
-            break
-        else
-            printf "."
-        fi
-        counter=$((counter+1))
-        # Wait a second
-        sleep 1
-    done
+publish_changelog() {
+    git-chglog --version
+    vro publish-changelog \
+        "${PARAM_CHANGELOG_FILE}" \
+        "${PARAM_MAIN_TRUNK_BRANCH}" \
+        "${PARAM_WORKING_DIRECTORY}"
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"${ORB_TEST_ENV}"}" = "$0" ]; then
-    MergeChangelog
+    publish_changelog
 fi
--- src/scripts/tag-and-release.sh
+++ src/scripts/tag-and-release.sh
@@ -1,94 +1,13 @@
-TagAndRelease() {
-    # pre-checks
-    if [ -n "${PARAM_GH_TOKEN_VAR}" ] && [ "${PARAM_GH_TOKEN_VAR}" != "GH_TOKEN" ]; then
-        echo "Setting the GH_TOKEN environment variable"
-        export GH_TOKEN="${!PARAM_GH_TOKEN_VAR}"
-    fi
-
-    # require a GH_TOKEN
-    if [ -z "${GH_TOKEN}" ]; then
-        echo "No GitHub write token found."
-        echo "Please set the environment variable GH_TOKEN."
-        echo "You can also specify which environment variable to use, see \"gh_token_var\" parameter in the tag-and-release job."
-        exit 1
-    fi
-
-    git fetch --all -p
-
-    if [ -f "${PARAM_COMMIT_FILE}" ]; then
-        currCommit=$(<"${PARAM_COMMIT_FILE}")
-        if [ "${currCommit}" != "" ]; then
-          echo "resetting to commit ${currCommit}"
-          git reset --hard "${currCommit}"
-        fi
-    fi
-
-    changelogUpdated=$(git diff --name-only -- "${PARAM_CHANGELOG_FILE}")
-    echo "changelogUpdated=${changelogUpdated}"
-    # Skip if there are changes in the changelog that have not been merged.
-    if [ -n "${changelogUpdated}" ]; then
-        echo "exiting, changes detected in the ${PARAM_CHANGELOG_FILE} file"
-        # ensure this file exist so persist does not fail.
-        echo "" > "${PARAM_TAG_FILE}"
-        exit 0
-    fi
-
-    hasTag=$(git show-ref "${CIRCLE_SHA1}" || echo "not found")
-    echo "hasTag=${hasTag}"
-    # Skip if this commit is already tagged.
-    if [ "${hasTag}" != "not found" ]; then
-        echo "exiting, commit is already tagged: ${hasTag}"
-        # ensure this file exist so persist does not fail.
-        echo "" > "${PARAM_TAG_FILE}"
-        exit 0
-    fi
-
-    git-tool-belt semver -save build-version.json
-    nextVersion=$(jq -r .nextVersion < build-version.json)
-    currVersion=$(jq -r .currentVersion < build-version.json)
-    releaseDay=$(date +"%Y-%m-%d")
-    revRange="${currVersion}"
-    # currVersion defaults to "HEAD" if it cannot find a tag, grabbing and tagging the whole history up to this point
-    if [ "${currVersion}" != "HEAD" ]; then
-        revRange="${currVersion}..HEAD"
-    fi
-
-    # Fetch all the refs
-    isTaggable=$(git-tool-belt taggable --commitRange "${revRange}")
-    echo "commit range ${revRange} tag ability is \"${isTaggable}\""
-    # Skip if there are no notable commits to tag.
-    if [ "${isTaggable}" = "false" ]; then
-        echo "exiting, no notable commits to tag"
-        # ensure this file exist so persist does not fail.
-        echo "" > "${PARAM_TAG_FILE}"
-        exit 0
-    fi
-
-    # Switch to SSH to use the token stored in the environment variable GH_TOKEN.
-    gh config set git_protocol ssh --host "${PARAM_GH_SERVER}"
-    echo
-    echo
-    # see: https://josh-ops.com/posts/gh-auth-login-in-actions/
-    # NOTE: Using just GH_TOKEN set in the environment seems to fail when you have
-    #       to supply the --hostname flag to point to a GHE server.
-    if [ "${PARAM_GH_SERVER}" != "github.com" ]; then
-        echo "login to ${PARAM_GH_SERVER}"
-        echo "${GH_TOKEN}" | gh auth login --hostname "${PARAM_GH_SERVER}" --with-token
-        echo
-        echo
-    fi
-    echo "auth status of ${PARAM_GH_SERVER}"
-    gh auth status --hostname "${PARAM_GH_SERVER}"
-    echo
-    echo
-    echo "tagging commit hash ${CIRCLE_SHA1} with ${nextVersion}"
-    gh release create "${nextVersion}" --generate-notes --target "${PARAM_BRANCH}" --title "${nextVersion} - ${releaseDay}"
-    echo "${nextVersion}" > "${PARAM_TAG_FILE}"
+publish_tag_and_release() {
+    vro publish-release-tag \
+        "${PARAM_CHANGELOG_FILE}" \
+        "${PARAM_MAIN_TRUNK_BRANCH}" \
+        "${PARAM_WORKING_DIRECTORY}"
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"${ORB_TEST_ENV}"}" = "$0" ]; then
-    TagAndRelease
+    publish_tag_and_release
 fi
