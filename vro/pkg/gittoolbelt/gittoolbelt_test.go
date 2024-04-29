6432e187582b626194488e0aa4e3ef76daa2f636
Khalifah K. Shabazz <shabazzk@gmail.com>
2024-04-29 01:51:37 -0400

    test: When Changes are Taggable
    Continue using git-tool-belt to check when a commit should be tagged.

--- vro/pkg/gittoolbelt/gittoolbelt_test.go
+++ vro/pkg/gittoolbelt/gittoolbelt_test.go
@@ -3,6 +3,7 @@ package gittoolbelt
 import (
 	help "github.com/kohirens/stdlib/test"
 	"os"
+	"testing"
 )
 
 const (
@@ -13,3 +14,25 @@ var (
 	tmpDir     = help.AbsPath("tmp")
 	fixtureDir = "testdata"
 )
+
+func TestIsTaggable(t *testing.T) {
+	tests := []struct {
+		name   string
+		bundle string
+		want   bool
+	}{
+		{"is-taggable", "repo-02", true},
+		{"not-taggable", "repo-03", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := help.SetupARepository(tt.bundle, tmpDir, fixtureDir, ps)
+			got := IsTaggable(repo)
+
+			if got != tt.want {
+				t.Errorf("IsTaggable() want %v, got %v", got, tt.want)
+				return
+			}
+		})
+	}
+}
