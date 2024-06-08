7822197278fdf432a7ab617d72753010b2472b77
Khalifah K. Shabazz <619281+b01@users.noreply.github.com>
2024-06-08 00:15:25 -0400

    test: Updated
    Modified according to prebvios refactorings.

--- vro/git_test.go
+++ vro/git_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/kohirens/stdlib/fsio"
 	"github.com/kohirens/stdlib/git"
 	"testing"
 )
@@ -14,6 +15,9 @@ func TestIsChangelogUpToDate(t *testing.T) {
 	}{
 		{"has-changes", "CHANGELOG.md", "repo-04", false},
 		{"no-changes", "CHANGELOG.md", "repo-05", true},
+		{"repo-has-chglog-changes", "CHANGELOG.md", "repo-has-chglog-changes", false},
+		{"repo-has-no-chglog-changes", "CHANGELOG.md", "repo-has-no-chglog-changes", true},
+		{"repo-no-previous-tag", "CHANGELOG.md", "repo-no-previous-tag", false},
 	}
 
 	for _, tt := range tests {
@@ -23,6 +27,10 @@ func TestIsChangelogUpToDate(t *testing.T) {
 			got, _ := IsChangelogUpToDate(repoDir, tt.chgLogFile)
 			if got != tt.want {
 				t.Errorf("IsChangelogUpToDate() = %v, want %v", got, tt.want)
+			}
+
+			if !fsio.Exist(repoDir + "/cliff.toml") {
+				t.Errorf("cliff.toml does not exist")
 			}
 		})
 	}
