5351e4eb138df3b13e29e0e50483a03784f115c6
Khalifah K. Shabazz <619281+b01@users.noreply.github.com>
2024-06-15 13:32:15 -0400

    fix: Output When Setting Version Info From A File
    Change text from "was pull from file" to "was pulled from the file."

--- src/scripts/publish-docker-hub.sh
+++ src/scripts/publish-docker-hub.sh
@@ -23,7 +23,7 @@ fi
 
 if [ -n "${PARAM_TAG_FILE}" ]; then
     semver="$(cat "${PARAM_TAG_FILE}")"
-    echo "semantic version ${semver} was pull from file ${PARAM_TAG_FILE}"
+    echo "semantic version ${semver} was pulled from the file ${PARAM_TAG_FILE}"
 fi
 
 # For backward compatibility.
