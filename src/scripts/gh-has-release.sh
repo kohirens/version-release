17b641c4780c49b63aea6f43dc1117e5034144bb
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2024-06-27 21:46:29 -0400

    feat: Command To Verify GitHub Release
    In addition to the job, it is also now a command.

--- src/scripts/gh-has-release.sh
+++ src/scripts/gh-has-release.sh
@@ -20,10 +20,30 @@ has_release() {
     fi
 }
 
-
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"${ORB_TEST_ENV}"}" = "$0" ]; then
-    has_release "${PARAM_OWNER}" "${PARAM_REPO}" "${PARAM_TAG}" > "${PARAM_FILE}"
+    semver=""
+    if [ -n "${PARAM_TAG_CMD}" ]; then
+        semver="$("${PARAM_TAG_CMD}")"
+        echo "release tag ${semver} was set by command"
+    fi
+
+    if [ -n "${PARAM_TAG_ENV_VAR}" ]; then
+        semver="${!PARAM_TAG_ENV_VAR}"
+        echo "release tag ${semver} was extracted from environment variable ${PARAM_TAG_ENV_VAR}"
+    fi
+
+    if [ -n "${PARAM_TAG_FILE}" ]; then
+        semver="$(cat "${PARAM_TAG_FILE}")"
+        echo "release tag ${semver} was pulled from the file ${PARAM_TAG_FILE}"
+    fi
+
+    if [ -z "${semver}" ]; then
+        echo "please set either parameter tag_cmd, tag_env_var, or tag_file to set the release tag to lookup"
+        exit 1
+    fi
+
+    has_release "${PARAM_OWNER}" "${PARAM_REPO}" "${semver}" > "${PARAM_FILE}"
 fi
